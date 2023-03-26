badc81e9f14a48cd98fdfd8508c9c6d05c5e0772
melkit235 <verghel.iliya@gmail.com>
2023-03-26 14:42:43 +0300

    lab2

---
+++ TI2/TI2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TI2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bOpen = new System.Windows.Forms.Button();
+            this.tKey = new System.Windows.Forms.TextBox();
+            this.rEncript = new System.Windows.Forms.RichTextBox();
+            this.bSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rFile = new System.Windows.Forms.RichTextBox();
+            this.rKey = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(179, 470);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(75, 23);
+            this.bOpen.TabIndex = 0;
+            this.bOpen.Text = "Открыть";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tKey
+            // 
+            this.tKey.Location = new System.Drawing.Point(21, 43);
+            this.tKey.Name = "tKey";
+            this.tKey.Size = new System.Drawing.Size(715, 20);
+            this.tKey.TabIndex = 2;
+            // 
+            // rEncript
+            // 
+            this.rEncript.Location = new System.Drawing.Point(480, 187);
+            this.rEncript.Name = "rEncript";
+            this.rEncript.Size = new System.Drawing.Size(422, 277);
+            this.rEncript.TabIndex = 3;
+            this.rEncript.Text = "";
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(661, 470);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(75, 23);
+            this.bSave.TabIndex = 4;
+            this.bSave.Text = "Сохранить";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Начальное состояние регистра ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(21, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Исходный файл";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(480, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Зашифрованный файл";
+            // 
+            // rFile
+            // 
+            this.rFile.Location = new System.Drawing.Point(21, 187);
+            this.rFile.Name = "rFile";
+            this.rFile.Size = new System.Drawing.Size(422, 277);
+            this.rFile.TabIndex = 8;
+            this.rFile.Text = "";
+            // 
+            // rKey
+            // 
+            this.rKey.Location = new System.Drawing.Point(21, 90);
+            this.rKey.Name = "rKey";
+            this.rKey.Size = new System.Drawing.Size(881, 68);
+            this.rKey.TabIndex = 9;
+            this.rKey.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(21, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ключ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rKey);
+            this.Controls.Add(this.rFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.rEncript);
+            this.Controls.Add(this.tKey);
+            this.Controls.Add(this.bOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.RichTextBox rKey;
+
+        private System.Windows.Forms.RichTextBox rFile;
+
+        private System.Windows.Forms.RichTextBox rDecript;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.RichTextBox rEncript;
+        private System.Windows.Forms.TextBox tKey;
+
+        #endregion
+    }
+}
---
+++ TI2/TI2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace TI2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        byte[] ResultBytes;
+        [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
+        async Task WriteKey(string key, RichTextBox r )
+        {
+            r.AppendText(key);
+        }
+        [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
+        async Task WriteFile(BitArray bitF, RichTextBox r)
+        {
+            string bitString = "";
+            for (int i = 0; (i < bitF.Length); i++)
+            {
+                bitString += bitF[i] ? "1" : "0";
+                if (i % 2000 == 0)
+                {
+                    r.AppendText(bitString);
+                    bitString = string.Empty;
+                }
+                        
+            }
+            r.AppendText(bitString);
+        }
+        
+        
+        
+        static BitArray BitsSift;
+        static bool ShiftAndAdd()
+        {
+            bool newBit = BitsSift[0] ^ BitsSift[25];
+            bool save = BitsSift[0];
+            for (int i = 0; i <BitsSift.Length-1; i++)
+            {
+                BitsSift[i] = BitsSift[i + 1];
+            }
+            BitsSift[35] = newBit;
+            return save;
+        }
+        
+        BitArray ReadBit(string key, byte[] bytes)
+        {
+            BitArray bitsFile = new BitArray(bytes);
+            
+            BitsSift = new BitArray(key.Select(c => c == '1').ToArray());
+
+            bool v;
+            int z1000=0;
+            string BitsKey = "";
+            for (int i = 0; i < bitsFile.Length; i++)
+            {
+                v = ShiftAndAdd();  
+                if (z1000 == 1000)
+                {
+                    WriteKey(BitsKey,rKey);
+                    BitsKey = string.Empty;
+                    z1000 = 0;
+                }
+                
+                bitsFile[i] ^= v ;
+                BitsKey += v ? "1" : "0";
+                z1000++;
+            }
+            rKey.AppendText(BitsKey);
+
+            return bitsFile;
+        }
+
+        
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = tKey.Text; 
+            key = new string(key.Where(c => c == '0' || c == '1').ToArray());
+            if (key.Length == 36)
+            {
+                
+                rFile.Text = string.Empty;
+                rEncript.Text = string.Empty;
+                rKey.Text = string.Empty;
+                    
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "C://Users//User//RiderProjects//TI2//TI2//bin//Debug";
+                
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    byte[] bytes = File.ReadAllBytes(openFileDialog.FileName);
+
+                    BitArray bitF = new BitArray(bytes);
+
+                    WriteFile(bitF,rFile);
+                    
+                    BitArray ResultBits = ReadBit(key, bytes);
+
+                    WriteFile(ResultBits, rEncript);
+
+                    ResultBytes = new byte[(ResultBits.Length - 1) / 8 + 1];
+                    ResultBits.CopyTo(ResultBytes, 0);
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Неверная длинна ключа");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = "C://Users//User//RiderProjects//TI2//TI2//bin//Debug";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, ResultBytes);
+            }
+            else
+            {
+                MessageBox.Show("Файл не выбран");
+            }
+        }
+    }
+}
+
+
+//101010101010101010101010101010101010
+//111111111111111111111111111111111111
+//000000001111111111111111111111111111
